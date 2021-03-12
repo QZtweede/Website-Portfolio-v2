7abe79147bc52e79bb9f9a019d25d4b4104b972e
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-03-12 15:30:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,84 @@ let Button2 = document.getElementById("B2")
 Button1.addEventListener("click", B1Clicked)
 Button2.addEventListener("click", B2Clicked)
 
+let LU1 = document.getElementById("LU1")
+let LU2 = document.getElementById("LU2")
+let LU3 = document.getElementById("LU3")
+let LU4 = document.getElementById("LU4")
+let LU5 = document.getElementById("LU5")
+let LU6 = document.getElementById("LU6")
+
 var counter = 0
-var LUs = ["LU1", "LU2", "LU3", "LU4", "LU5", "LU6"]
+
+
+LU1.style.display = "block"
+LU2.style.display="none"
+LU3.style.display="none"
+LU4.style.display="none"
+LU5.style.display="none"
+LU6.style.display="none"
 
 function B1Clicked(){
     counter++
     if(counter >= 6){
         counter = 0
     }
-    console.log(counter)
+    Opacity()
 }
 
 function B2Clicked(){
     counter--
     if(counter <= -1){
         counter = 5
+    }  
+    Opacity()
+}
+
+function Opacity(){
+    Reset()
+    
+    function Reset(){
+        LU1.style.display="none"
+        LU2.style.display="none"
+        LU3.style.display="none"
+        LU4.style.display="none"
+        LU5.style.display="none"
+        LU6.style.display="none"
     }
-    console.log(counter)
+
+    if(counter == 0){
+        Reset()
+        LU1.style.display = "block"
+        console.log(counter)
+    }
+
+    if(counter == 1){
+        Reset()
+        LU2.style.display = "block"
+        console.log(counter)
+    }
+
+    if(counter == 2){
+        Reset()
+        LU3.style.display = "block"
+        console.log(counter)
+    }
+
+    if(counter == 3){
+        Reset()
+        LU4.style.display = "block"
+        console.log(counter)
+    }
+
+    if(counter == 4){
+        Reset()
+        LU5.style.display = "block"
+        console.log(counter)
+    }
+
+    if(counter == 5){
+        Reset()
+        LU6.style.display = "block"
+        console.log(counter)
+    }
 }
