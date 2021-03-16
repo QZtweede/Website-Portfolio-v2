024774d651d39a35301f3a1d41625e8eaf395114
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-03-16 10:01:10 +0100

    Update pointer.js

--- pointer.js
+++ pointer.js
@@ -1,5 +1,7 @@
 let ani = document.getElementById("Cursor-animation2")
 
+let ani2 = document.getElementById("Cursor-animation3")
+
 let curs = document.getElementById("Cursor-animation")
 document.addEventListener("mousemove", function(i){
     curs.style.left = i.clientX + "px"
@@ -8,9 +10,11 @@ document.addEventListener("mousemove", function(i){
 
 async function onClick(){
     ani.classList.toggle("animate")
-    console.log("scale")
+    await sleep(100)
+    ani2.classList.toggle("animate2")
     await sleep(300)
     ani.classList.toggle("animate")
+    ani2.classList.toggle("animate2")
 }
 
 function sleep(time) {
