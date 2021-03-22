366423211222f46f195ef7bc28f6cf0eb4d58e3e
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-03-22 10:07:53 +0100

    Update pointer.js

--- pointer.js
+++ pointer.js
@@ -2,6 +2,11 @@ let ani = document.getElementById("Cursor-animation2")
 
 let ani2 = document.getElementById("Cursor-animation3")
 
+let hover = document.getElementById("Hover-text")
+
+let portfolioTxt = document.getElementById("portfolioTxt")
+
+
 let curs = document.getElementById("Cursor-animation")
 document.addEventListener("mousemove", function(i){
     curs.style.left = i.clientX + "px"
@@ -20,3 +25,28 @@ async function onClick(){
 function sleep(time) {
     return new Promise(resolve => setTimeout(resolve, time));
   }
+
+function opacityIn(){
+    console.log("test")
+    hover.style.opacity=1
+}
+
+function opacityOut(){
+    console.log("test2")
+    hover.style.opacity=0
+}
+
+async function japaneseIn(){
+    portfolioTxt.innerHTML="ポートフォリオ"
+    portfolioTxt.style.fontSize="70px"
+    portfolioTxt.style.right="80px"
+    portfolioTxt.style.top="30px"
+}
+
+async function japaneseOut(){
+    await sleep(200)
+    portfolioTxt.innerHTML="Portfolio"
+    portfolioTxt.style.fontSize="120px"
+    portfolioTxt.style.right="200px"
+    portfolioTxt.style.top="10px"
+}
