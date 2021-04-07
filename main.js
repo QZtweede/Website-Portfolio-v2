e7fdeb3877c6dd50d0b72fd4b6b9b8e079661e95
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-04-07 11:48:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ let LU4 = document.getElementById("LU4")
 let LU5 = document.getElementById("LU5")
 let LU6 = document.getElementById("LU6")
 
-var counter = 0
+let D1 = document.getElementById("D1")
+let D2 = document.getElementById("D2")
+let D3 = document.getElementById("D3")
+let D4 = document.getElementById("D4")
+let D5 = document.getElementById("D5")
+let D6 = document.getElementById("D6")
 
+var counter = 0
 
 LU1.style.opacity = 1
 LU2.style.opacity = 0
@@ -19,6 +25,8 @@ LU3.style.opacity = 0
 LU4.style.opacity = 0
 LU5.style.opacity = 0
 LU6.style.opacity = 0
+
+D1.style.background = "white"
 
 LU1.style.pointerEvents = "all"
 LU2.style.pointerEvents = "none"
@@ -60,12 +68,20 @@ function Opacity(){
     LU4.style.pointerEvents = "none"
     LU5.style.pointerEvents = "none"
     LU6.style.pointerEvents = "none"
+
+    D1.style.background = "#696969"
+    D2.style.background = "#696969"
+    D3.style.background = "#696969"
+    D4.style.background = "#696969"
+    D5.style.background = "#696969"
+    D6.style.background = "#696969"
     }
 
     if(counter == 0){
         Reset()
         LU1.style.opacity = 1
         LU1.style.pointerEvents = "all"
+        D1.style.background = "white"
         console.log(counter)
     }
 
@@ -73,6 +89,7 @@ function Opacity(){
         Reset()
         LU2.style.opacity = 1
         LU2.style.pointerEvents = "all"
+        D2.style.background = "white"
         console.log(counter)
     }
 
@@ -80,6 +97,7 @@ function Opacity(){
         Reset()
         LU3.style.opacity = 1
         LU3.style.pointerEvents = "all"
+        D3.style.background = "white"
         console.log(counter)
     }
 
@@ -87,6 +105,7 @@ function Opacity(){
         Reset()
         LU4.style.opacity = 1
         LU4.style.pointerEvents = "all"
+        D4.style.background = "white"
         console.log(counter)
     }
 
@@ -94,6 +113,7 @@ function Opacity(){
         Reset()
         LU5.style.opacity = 1
         LU5.style.pointerEvents = "all"
+        D5.style.background = "white"
         console.log(counter)
     }
 
@@ -101,6 +121,7 @@ function Opacity(){
         Reset()
         LU6.style.opacity = 1
         LU6.style.pointerEvents = "all"
+        D6.style.background = "white"
         console.log(counter)
     }
 }
